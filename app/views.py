29b427573ef930691d61ecdd3a1a33d9abd6611c
jjdale1999 <dalejordanne@gmail.com>
2020-02-04 13:07:03 -0500

    layout completed

--- app/views.py
+++ app/views.py
@@ -7,6 +7,11 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
+
+def format_date_joined(dat):
+    # year,month,date= datetime.datetime.strptime(dat, "%Y-%m-%d").split("-")
+    return  dat.strftime("%B, %Y") 
 
 
 ###
@@ -24,7 +29,9 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',date=format_date_joined(datetime.datetime.now()),name="Mary Jane",username="mjane")
 ###
 # The functions below should be applicable to all Flask apps.
 ###
